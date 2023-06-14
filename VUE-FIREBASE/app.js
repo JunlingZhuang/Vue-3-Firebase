fb0925c277ded1cf53e961c0a87b7b61da8295d3
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2023-06-13 23:35:53 -0400

    update

--- VUE-FIREBASE/app.js
+++ VUE-FIREBASE/app.js
@@ -2,14 +2,26 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The fianl Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      books: [
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "the way of kings", author: "brandon sanderson" },
+        { title: "the final empire", author: "patrick rothfuss" },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
