fab3179e91c88210dd5d88ad3ca8b7c99e33e231
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2023-06-15 20:13:53 -0400

    update computed

--- VUE-FIREBASE/app.js
+++ VUE-FIREBASE/app.js
@@ -1,11 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.junling-design.com/",
       showBooks: true,
       books: [
-        { title: "name of the wind", author: "patrick rothfuss" },
-        { title: "the way of kings", author: "brandon sanderson" },
-        { title: "the final empire", author: "patrick rothfuss" },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "patrick rothfuss",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
       ],
     };
   },
@@ -13,15 +29,24 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
+    toggleFav(book) {
+      book.isFav = !book.isFav;
     },
-    handleMousemove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
+    // handleEvent(e, data) {
+    //   console.log(e, e.type);
+    //   if (data) {
+    //     console.log(data);
+    //   }
+    // },
+    // handleMousemove(e) {
+    //   this.x = e.offsetX;
+    //   this.y = e.offsetY;
+    // },
+  },
+
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
